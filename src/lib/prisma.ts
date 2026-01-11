a86400b7af9816b43485f6516e80be4bc81006a0
RyanNeuville <neuvilleryan123@gmail.com>
2026-01-11 09:56:27 +0100

    Update prisma client to use singleton patterns

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,13 @@
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '../generated/prisma';
 
-const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL })
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
